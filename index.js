5e2dd4cc0fcb35b45a100b52d608b3d83c950ab1
Todd <toddtaylor189@gmail.com>
2024-04-11 19:04:04 +0100

    final commit of Thursday

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const Discord = require('discord.js')
 const {OpenAI}= require('openai')
 require('dotenv/config')
 
-const client = new Discord.Client()
+const client = new Discord.Client();
+client.login('DISCORD_API');
